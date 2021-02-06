11c51cb028ead136e0c895240ceea09a38ecf558
MZ <mchlzj@gmail.com>
2021-02-06 14:47:45 +0100

    Post und Get Requests hinzugefügt. Get funktioniert. Post noch nicht. Hängt irgendwie mit den Cors Einstellungen und dem Preflight Request zusammen.

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CRUD from './CRUD';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <CRUD />
   </React.StrictMode>,
   document.getElementById('root')
 );
