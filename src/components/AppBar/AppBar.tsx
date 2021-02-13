5a5b2ec084a82771749055d651399bd4e94c3010
MZ <mchlzj@gmail.com>
2021-02-13 02:23:58 +0100

    Erste Gehversuche mit Material UI und React Router. Aus Material UI Komponenten wurden eigene Komponenten gebaut. Daraus ist ein Drawer entstanden, der stetig unter der AppBar auftaucht und responsiv ist. Dafür habe ich die AppBar in den Vordergrund gerückt. Dadurch verschwindet allerdings leider das Menü, wenn auf das Profil-Icon geklickt wird. Muss noch gefixt werden. Aber irgendwie will das mit z-index Einstellungen nicht so recht funktionieren.

---
+++ src/components/AppBar/AppBar.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Badge from '@material-ui/core/Badge';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MenuUser, {UserMenuId} from './MenuUser';
+
+
+const drawerWidth = 240;
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    title: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+          display: 'flex',
+          flexGrow: 1,
+          align: 'center'
+        },
+      },
+      sectionDesktop: {
+        display: 'flex',
+      },
+      grow: {
+        flexGrow: 1,
+      },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 101,
+      // [theme.breakpoints.up('md')]: {
+      //   width: `calc(100% - ${drawerWidth}px)`,
+      //   marginLeft: drawerWidth,
+      // },
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up('md')]: {
+        display: 'none',
+      },
+    },
+  }),
+);
+
+
+function StyledAppBar({mobileOpen, setMobileOpen}: any) {
+  const classes = useStyles();
+  // Auskommentiert, da sich der Z-Index des Menüs irgendwie nicht ändern lässt
+  // const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  //   const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+  //   setAnchorEl(event.currentTarget);
+  // };
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+    console.log("cklicked");
+  };
+
+  return (
+    <div>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Easy-Ticket
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <IconButton aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton aria-label="show 17 new notifications" color="inherit">
+              <Badge badgeContent={17} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={UserMenuId}
+              aria-haspopup="true"
+              // onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {/* Irgendwie lässt sich der Z-Index des Menüs nicht ändern. Deshalb erstmal auskommentiert. */}
+      {/* <MenuUser
+        anchorEl={anchorEl}
+        setAnchorEl={setAnchorEl}/> */}
+    </div>
+  );
+}
+
+export default StyledAppBar;
