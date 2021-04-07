284efa42086e3d42628e55af5f965410d10847b1
Alex <alexanderk09@web.de>
2021-04-07 20:06:51 +0200

    mui style theme implemented

--- src/components/SearchBar/TicketSearchBar.tsx
+++ src/components/SearchBar/TicketSearchBar.tsx
@@ -1,7 +1,6 @@
 import {useState} from 'react';
 import Grid from '@material-ui/core/Grid';
 import Switch from '@material-ui/core/Switch';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import Divider from '@material-ui/core/Divider';
@@ -11,32 +10,8 @@ import SearchIcon from '@material-ui/icons/Search';
 // import DirectionsIcon from '@material-ui/icons/Directions';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 // import FormControl from '@material-ui/core/FormControl';
+import useStyles from '../../mui-style-theme';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      padding: '2px 4px',
-      display: 'flex',
-      alignItems: 'center',
-      width: 400,
-      marginBottom: theme.spacing(1),
-      position: 'sticky',
-      top: 90,
-      zIndex: 20
-    },
-    input: {
-      marginLeft: theme.spacing(1),
-      flex: 1,
-    },
-    searchIcon: {
-      marginLeft: 10,
-    },
-    divider: {
-      height: 4, 
-      margin: 4
-    },
-  }),
-);
 
 export default function TicketSearchBar() {
   const classes = useStyles();
